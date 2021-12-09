7371092a966e51a49ca18b6b775f8a74e20d3c13
Tushar <Tushar@Tushar.mshome.net>
2021-12-09 15:18:28 +0530

    Cyclic/Circular Injection using two-way setter methods :: DefaultListableBeanFactory

---
+++ IOC_05_Cyclic_Circular_Dual-Setter-Injection/src/main/java/com/tushar/beans/A.java
@@ -0,0 +1,16 @@
+package com.tushar.beans;
+
+public class A {
+
+	private B b;
+
+	public void setB(B b) {
+		this.b = b;
+	}
+	
+	@Override
+	public String toString() {
+		return "Welcome to Class A";
+	}
+	
+}
---
+++ IOC_05_Cyclic_Circular_Dual-Setter-Injection/src/main/java/com/tushar/beans/B.java
@@ -0,0 +1,19 @@
+package com.tushar.beans;
+
+public class B {
+
+	private A a;
+
+	public void setA(A a) {
+		this.a = a;
+	}
+
+	@Override
+	public String toString() {
+		return "Welcome to Class B";
+	}
+	
+	
+	
+	
+}
